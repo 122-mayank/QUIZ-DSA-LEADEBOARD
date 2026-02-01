26b4dbda123bf379410e5c225263cf8dc3598066
MAYANK SAINI <Mayank Saini>
2026-02-01 15:32:12 +0530

    Adding values

--- Public/script/server.js
+++ Public/script/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./firebase');
-const { messaging } = require('firebase-admin');
+// const { messaging } = require('firebase-admin');
 const session = require('express-session');
 const app = express();
 
